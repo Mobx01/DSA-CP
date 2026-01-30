d7642bdb25795ae5af01b8d3ce059ee806823bd5
ANANT BHARDWAJ <102032968+Mobx01@users.noreply.github.com>
2026-01-30 15:25:41 +0530

    Update rem-dup-from-sorted-array.cpp

--- rem-dup-from-sorted-array.cpp
+++ rem-dup-from-sorted-array.cpp
@@ -1,4 +1,8 @@
-/*
+/*Leetcode PS- 26 Remove Duplicates from Sorted Array
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+
 Approach:
 - Since the array is sorted, all duplicate elements appear consecutively.
 - We use the two-pointer technique to remove duplicates in-place.
