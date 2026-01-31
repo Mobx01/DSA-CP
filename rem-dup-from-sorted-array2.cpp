4037d27f308d70d2a5e12d62da16ad629518a466
ANANT BHARDWAJ <102032968+Mobx01@users.noreply.github.com>
2026-01-31 15:18:15 +0530

    Create rem-dup-from-sorted-array2.cpp

---
+++ rem-dup-from-sorted-array2.cpp
@@ -0,0 +1,24 @@
+/*Leetcode Ps- 80. Remove Duplicates from Sorted Array II
+Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates,
+then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+Return k after placing the final result in the first k slots of nums.
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.*/
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        if(nums.size() == 1) return 1 ;
+
+        int i = 2; 
+
+        for (int j = 2; j < nums.size(); j++) {
+            if (nums[j] != nums[i-2]) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+};
