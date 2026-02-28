60223ccba36a0f4635e3b17b520aacf45b6f4659
ANANT BHARDWAJ <102032968+Mobx01@users.noreply.github.com>
2026-02-28 20:59:42 +0530

    Create concatenate-consecutive-bin-num.cpp

---
+++ concatenate-consecutive-bin-num.cpp
@@ -0,0 +1,43 @@
+/*Leetcode:- 1680. Concatenation of Consecutive Binary Numbers
+Given an integer n, return the decimal value of the binary string formed by concatenating the binary representations of 1 to n in order,
+modulo 109 + 7.*/
+
+/*Non optimal solution*/
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+            result = char(sum % 2 + '0') + result;
+            carry = sum / 2;
+        }
+        return result;
+    }
+
+    int concatenatedBinary(int n) {
+        string s = "";
+        string step = "0";
+        int count = 0;
+
+        while (count < n) {
+            step = addBinary(step, "1");
+            s += step; // append the binary string
+            count++;
+        }
+
+        long long result = 0;
+        long long mod = 1e9 + 7;
+        
+        for (char c : s) {
+            result = (result * 2 + (c - '0')) % mod;
+        }
+        return (int)result;
+    }
+};
+
+
+/*Optimal solution*/
