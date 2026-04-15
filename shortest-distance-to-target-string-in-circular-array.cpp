d6793ad4cdb23ca0edc2b858a5bb8aaba50a7c37
ANANT BHARDWAJ <102032968+Mobx01@users.noreply.github.com>
2026-04-15 17:15:57 +0530

    Create shortest-distance-to-target-string-in-circular-array.cpp

---
+++ shortest-distance-to-target-string-in-circular-array.cpp
@@ -0,0 +1,18 @@
+/*Leetcode ps-2515. Shortest Distance to Target String in a Circular Array
+You are given a 0-indexed circular string array words and a string target. A circular array means that the array's end connects to the array's beginning.
+Formally, the next element of words[i] is words[(i + 1) % n] and the previous element of words[i] is words[(i - 1 + n) % n], where n is the length of words.
+Starting from startIndex, you can move to either the next word or the previous word with 1 step at a time.
+Return the shortest distance needed to reach the string target. If the string target does not exist in words, return -1.*/
+
+
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int startIndex) {
+        int n = words.size();
+        for(int i =0;i<n;i++){
+            if(words[(startIndex+i)%n] == target) return i;
+            else if(words[(startIndex-i+n)%n] == target) return i;
+        }
+        return -1;
+    }
+};
