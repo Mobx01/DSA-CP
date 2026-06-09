cd185395f5bbd5f6ac619be9ce02ec35fa853fc8
ANANT BHARDWAJ <102032968+Mobx01@users.noreply.github.com>
2026-06-09 13:23:12 +0530

    Create max-total-subarray-value-I.cpp

---
+++ python/max-total-subarray-value-I.cpp
@@ -0,0 +1,25 @@
+/*Leetcode ps- 3689. Maximum Total Subarray Value I
+You are given an integer array nums of length n and an integer k.
+You need to choose exactly k non-empty subarrays nums[l..r] of nums. Subarrays may overlap, and the exact same subarray (same l and r) can be chosen more than once.
+The value of a subarray nums[l..r] is defined as: max(nums[l..r]) - min(nums[l..r]).
+The total value is the sum of the values of all chosen subarrays.
+Return the maximum possible total value you can achieve.*/
+
+/*
+APPROACH: 
+1. Setup: Start `maxn` at the lowest possible number (INT_MIN) and 
+   `minn` at the highest (INT_MAX) so they update properly.
+2. Single Pass: Loop through the array exactly once. Update `maxn` 
+   if you find a bigger number, and `minn` if you find a smaller one.
+3. Math & Safety: Subtract the min from the max, and multiply by 'k'. 
+   Multiply by `1LL` first to force 64-bit math—this prevents the 
+   final huge number from crashing the standard 32-bit integer limit!
+*/
+
+
+class Solution:
+    def maxTotalValue(self, nums: List[int], k: int) -> int:
+        maxn = max(nums)
+        minn = min(nums)
+        
+        return k * (maxn - minn)
